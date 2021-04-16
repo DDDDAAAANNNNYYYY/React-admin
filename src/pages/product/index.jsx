ce689d54d1b2f54d9fb0db711318e6545ea4358b
ychen800 <yc3753@columbia.edu>
2021-04-15 23:48:48 -0700

    commit

--- src/pages/product/index.jsx
+++ src/pages/product/index.jsx
@@ -118,8 +118,36 @@ export default class Product extends Component {
     }
 
     setDefault = async() => {
-
+        let newUser = null;
+        axios.get("/api/user/getUser/" + this.state.username).then(res=>{
+            newUser = res.data;
+            let temp = this.state.timeSheetUnit.week;
+            let template = [
+                {startTime: temp[0].startTime + '', endTime: temp[0].endTime + '', vacation: temp[0].vacation + '', floatingDay: temp[0].floatingDay + ''},
+                {startTime: temp[1].startTime + '', endTime: temp[1].endTime + '', vacation: temp[1].vacation + '', floatingDay: temp[1].floatingDay + ''},
+                {startTime: temp[2].startTime + '', endTime: temp[2].endTime + '', vacation: temp[2].vacation + '', floatingDay: temp[2].floatingDay + ''},
+                {startTime: temp[3].startTime + '', endTime: temp[3].endTime + '', vacation: temp[3].vacation + '', floatingDay: temp[3].floatingDay + ''},
+                {startTime: temp[4].startTime + '', endTime: temp[4].endTime + '', vacation: temp[4].vacation + '', floatingDay: temp[4].floatingDay + ''},
+                {startTime: temp[5].startTime + '', endTime: temp[5].endTime + '', vacation: temp[5].vacation + '', floatingDay: temp[5].floatingDay + ''},
+                {startTime: temp[6].startTime + '', endTime: temp[6].endTime + '', vacation: temp[6].vacation + '', floatingDay: temp[6].floatingDay + ''}
+            ]
+            newUser.template = template;
+            axios.post("/api/user/addUser", newUser).then(res => {
+                alert("Template Saved!");
+            });
+        });
     }
+
+    onFileChangeHandler = (e) => {
+        e.preventDefault();
+        const username = this.state.username;
+        const formData = new FormData();
+        formData.append('file', e.target.files[0]);
+        axios.post("/api/user/upload/" + username, formData)
+            .then(res => {
+                alert("File uploaded successfully.")
+            })
+    };
 
     getFloatingDayComment() {
         let cnt = 0;
@@ -135,22 +163,18 @@ export default class Product extends Component {
 
     updateStatus = async () => {   
         // this.state;
-      
-       
-       this.setState(prevState => {
-           let st = Object.assign({}, prevState);
-           if (prevState.timeSheetUnit.approvalStatus == 'approved') {
-               st.timeSheetUnit.submissionStatus = 'complete';
-           }
-           else {
-               st.timeSheetUnit.submissionStatus = 'incomplete';
-           }
-           st.timeSheetUnit.comments = this.getFloatingDayComment() + this.getVacationComment();
-           return st;
-       });
+
+        let st = Object.assign({}, this.state.timeSheetUnit);
+        if (st.approvalStatus == 'approved') {
+            st.submissionStatus = 'complete';
+        }
+        else {
+            st.submissionStatus = 'incomplete';
+        }
+        st.comments = this.getFloatingDayComment() + this.getVacationComment();
         const username = memoryUtils.user.name
         console.log(this.state.timeSheetUnit.weekEnd);
-        axios.post("api/user/saveUser", this.state.timeSheetUnit).then( res=>{
+        axios.post("api/user/saveUser", st).then( res=>{
           alert("Updated successfully");
         })
     
@@ -164,7 +188,7 @@ export default class Product extends Component {
         const name = memoryUtils.user.name;
         const result = await reqUsers(name);
         this.setState({mylist: mylista[index]});
-        this.setState({timeSheetUnit: mylista[index]});
+        this.setState({timeSheetUnit: this.loadTimeSheetUnit(mylista[index])});
         //this.setState({mylist: mylista[index].week});
         const  users  = result;
         console.log("result", result);
@@ -173,6 +197,23 @@ export default class Product extends Component {
         this.setState({users: users});
         storageUtils.saveIndex(0);
     };
+
+    loadTimeSheetUnit(mylist) {
+        let timeSheetUnit = Object.assign({}, mylist);
+        let week = [];
+        timeSheetUnit.week.forEach((d, index) => {
+            week.push({
+                date: d.date,
+                startTime: parseFloat(d.startTime),
+                endTime: parseFloat(d.endTime),
+                floatingDay: d.floatingDay === 'true',
+                holiday: d.holiday === 'true',
+                vacation: d.vacation === 'true'
+            });
+        });
+        timeSheetUnit.week = week;
+        return timeSheetUnit;
+    }
 
     chooseDate(event) {
         let chosenDate = Date.parse(event.target.value);
@@ -188,14 +229,10 @@ export default class Product extends Component {
     }
 
     render() {
-        console.log(new Date('2021-04-17'));
         return (
             <div>
                 <div>
                     <div>
-                        {/* <div>
-                         <DatePicker selected={Date.parse(this.state.timeSheetUnit.week[6].date)} placeholderText="Select a Saturday" filterDate={date => date.getDay == 6} onChange={date => console.log(date)}/>
-                        </div> */}
                         <br/>
                         Week Ending <input type = 'date' value = {this.state.timeSheetUnit.week[6].date} onChange = {(e) =>{console.log("eeeee", e.target.value); this.chooseDate(e);}}></input>
                         Total Billing Hours <input type = 'text' value={this.getTotalBillingHours()} disabled></input>
@@ -219,7 +256,7 @@ export default class Product extends Component {
                             <td>{days[index]}</td>
                             <td>{date.toString()}</td>
                             <td>
-                                <select disabled={this.state.timeSheetUnit.approvalStatus == 'approved'} value={this.state.timeSheetUnit.week[index].startTime} onChange={(e) => this.setState(prevState => {let tsu = Object.assign({},prevState); tsu.timeSheetUnit.week[index].startTime = e.target.value; if (e.target.value === -1) {tsu.timeSheetUnit.week[index].endTime = -1;}; return tsu;})}>
+                                <select disabled={this.state.timeSheetUnit.submissionStatus == 'complete'} value={this.state.timeSheetUnit.week[index].startTime} onChange={(e) => this.setState(prevState => {let tsu = Object.assign({},prevState); tsu.timeSheetUnit.week[index].startTime = e.target.value; if (e.target.value === -1) {tsu.timeSheetUnit.week[index].endTime = -1;}; return tsu;})}>
                                     <option value={-1} selected = {startTime === -1}>N/A</option>
                                     {
                                         times.map((time, index) =>
@@ -230,7 +267,7 @@ export default class Product extends Component {
                                 </select>
                             </td>
                             <td>
-                                <select disabled={this.state.timeSheetUnit.approvalStatus == 'approved'} value={this.state.timeSheetUnit.week[index].endTime} onChange={(e) => this.setState(prevState => {let tsu = Object.assign({},prevState); tsu.timeSheetUnit.week[index].endTime = e.target.value; if (e.target.value === -1) {tsu.timeSheetUnit.week[index].startTime = -1;}; return tsu;})}>
+                                <select disabled={this.state.timeSheetUnit.submissionStatus == 'complete'} value={this.state.timeSheetUnit.week[index].endTime} onChange={(e) => this.setState(prevState => {let tsu = Object.assign({},prevState); tsu.timeSheetUnit.week[index].endTime = e.target.value; if (e.target.value === -1) {tsu.timeSheetUnit.week[index].startTime = -1;}; return tsu;})}>
                                     <option value={-1} selected = {startTime === -1}>N/A</option>
                                     {
                                         times.map((time, index) =>
@@ -242,27 +279,27 @@ export default class Product extends Component {
                             </td>
                             <td>{startTime === -1 || endTime === -1? 0 : (endTime - startTime)}</td>
                             <td>
-                                <input type = 'radio' name = {index} value = 'floatingDay' checked = {floatingDay == true} disabled={this.state.timeSheetUnit.approvalStatus == 'approved'} onChange={(e) => this.setState(prevState => {let st = Object.assign({}, prevState); st.timeSheetUnit.week[index].floatingDay = (e.target.value == 'floatingDay'); if (e.target.value == 'floatingDay'){st.timeSheetUnit.week[index].startTime = -1;st.timeSheetUnit.week[index].endTime =-1;}; return st;})}></input>
+                                <input type = 'radio' name = {index} value = 'floatingDay' checked = {floatingDay == true} disabled={this.state.timeSheetUnit.submissionStatus === 'complete' || this.state.timeSheetUnit.week[index].holiday == true} onChange={(e) => this.setState(prevState => {let st = Object.assign({}, prevState); st.timeSheetUnit.week[index].floatingDay = (e.target.value === 'floatingDay'); if (e.target.value === 'floatingDay'){st.timeSheetUnit.week[index].startTime = -1;st.timeSheetUnit.week[index].endTime =-1;}; return st;})}></input>
                             </td>
                             <td>
-                                <input type = 'radio' name = {index} value = 'holiday' checked = {holiday == true} disabled></input>
+                                <input type = 'radio' name = {index} value = 'holiday' checked = {holiday == true} disabled={true}></input>
                             </td>
                             <td>
-                                <input type = 'radio' name = {index} value = 'vacation' checked = {vacation == true} disabled={this.state.timeSheetUnit.approvalStatus == 'approved'} onChange={(e) => this.setState(prevState => {let st = Object.assign({}, prevState); st.timeSheetUnit.week[index].vacation = (e.target.value == 'vacation'); if (e.target.value == 'vacation'){st.timeSheetUnit.week[index].startTime = -1;st.timeSheetUnit.week[index].endTime =-1;}; return st;})}></input>
+                                <input type = 'radio' name = {index} value = 'vacation' checked = {vacation == true} disabled={this.state.timeSheetUnit.submissionStatus === 'complete' || this.state.timeSheetUnit.week[index].holiday == true} onChange={(e) => this.setState(prevState => {let st = Object.assign({}, prevState); st.timeSheetUnit.week[index].vacation = (e.target.value === 'vacation'); if (e.target.value === 'vacation'){st.timeSheetUnit.week[index].startTime = -1;st.timeSheetUnit.week[index].endTime =-1;}; return st;})}></input>
                             </td>
                         </tr>
                     )}
                     </tbody>
                 </table>
-                <button onClick={this.setDefault} disabled={this.state.timeSheetUnit.approvalStatus == 'approved'} title='Save daily hours as default; future weekly timesheet will show same hours.'>Set Default <AiFillInfoCircle></AiFillInfoCircle></button>
+                <button onClick={this.setDefault} disabled={this.state.timeSheetUnit.submissionStatus == 'complete'} title='Save daily hours as default; future weekly timesheet will show same hours.'>Set Default <AiFillInfoCircle style={{}}></AiFillInfoCircle></button>
                 <div>
-                    <select disabled={this.state.timeSheetUnit.approvalStatus == 'approved'} value={this.state.timeSheetUnit.approvalStatus} onChange={(e) => this.setState(prevState => {let st = Object.assign({}, prevState); st.timeSheetUnit.approvalStatus = e.target.value; console.log(this.state);return st;})}>
+                    <select disabled={this.state.timeSheetUnit.submissionStatus == 'complete'} value={this.state.timeSheetUnit.approvalStatus} onChange={(e) => this.setState(prevState => {let st = Object.assign({}, prevState); st.timeSheetUnit.approvalStatus = e.target.value; console.log(this.state);return st;})}>
                         <option value = 'approved'>Approved Timesheet</option>
                         <option value = 'not approved'>Unapproved Timesheet</option>
                     </select>
-                    <input type = 'file' disabled={this.state.timeSheetUnit.approvalStatus == 'approved'}></input>
+                    <input type = 'file' onChange={(e)=>{this.onFileChangeHandler(e)}} disabled={this.state.timeSheetUnit.submissionStatus == 'complete'}></input>
                 </div>
-                <button onClick={this.updateStatus} disabled={this.state.timeSheetUnit.approvalStatus == 'approved'}>Save</button>
+                <button onClick={this.updateStatus} disabled={this.state.timeSheetUnit.submissionStatus == 'complete'}>Save</button>
             </div>
             // <div>
             //     a:
